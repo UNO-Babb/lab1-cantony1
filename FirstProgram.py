ea149a641f56c4e9dfdd208cb10571b092e97080
cantony1 <cantony@unomaha.edu>
2024-09-01 20:13:28 +0000

    finished lab 1

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,16 +6,19 @@
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello")
   #Ask for the user's name
+  name = input("Enter your name: ")
 
   #Use the user's name in the program.
-
+  print("Nice to meet you", name)
   #Ask the user for their age.
-
+  age = input("Enter your age: ")
+  age = int(age)
   #Tell the user what year they were born in.
+  born = 2024 - age
   #Assume that they have not had their birthday yet this year.
-
+  print("You were born in", born)
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
--- MadLib.py
+++ MadLib.py
@@ -6,8 +6,16 @@
 def main():
   print("Madlib")
   #Ask user for words
+  adjective1 = input("Enter an adjective: ")
+  adjective2 = input("Enter another adjective: ")
+  meat1 = input("Enter a type of meat: ")
+  room1 = input("Enter name of a room: ")
+  verb1 = input("Enter a verb in past tense: ")
+  verb2 = input("Enter a verb: ")
 
   #Print the story with the user supplied words.
+  print(" It was a " + adjective1+ ", cold November day. I woke up to the " + adjective2 + " smell of " + meat1+ " roasting in the " + room1+ " downstairs. I " + verb1+ " down the stairs to see if I could help " + verb2+ " the dinner")
+
 
 
 
